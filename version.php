0a12fe95a1a0dded4c4d5b41934bd12ecfd1fcd7
Nuria Pacheco <npacheco@cert.org>
2026-02-17 21:38:50 +0000

    version bump

--- version.php
+++ version.php
@@ -42,7 +42,7 @@ DM24-1176
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2026020201;
+$plugin->version = 2026021700;
 $plugin->requires  = 2025041400;
 $plugin->component = 'block_crucible';
 $plugin->maturity = MATURITY_ALPHA;
